85d7cc87834dfa3cf1fddf3902b254da041a0438
Shir Kempinski <shirk9@walla.co.il>
2017-12-06 18:24:34 +0200

    update .gitignore

--- cmake-build-debug/CMakeFiles/ex3.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "ex3.pdb"
-  "ex3"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ex3.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
