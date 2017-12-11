a01ec6d75fe70afac984bccacc9320951e8c8896
shohamHadad <33810472+shohamHadad@users.noreply.github.com>
2017-12-11 17:17:49 +0200

    Update ClientPlayer.cpp

--- src/client/src/ClientPlayer.cpp
+++ src/client/src/ClientPlayer.cpp
@@ -8,13 +8,30 @@
 #include <unistd.h>
 
 using namespace std;
-
+/**
+ * function name: ClientPlayer
+ * input: the server IP, server port
+ * output: new object of ClientPlayer
+ * operation: constructor
+ */
 ClientPlayer::ClientPlayer(const char *serverIP, int serverPort): Client(serverIP, serverPort) {
 }
 
+/**
+ * function name: ~ClientPlayer
+ * input: void
+ * output: delete ClientPlayer
+ * operation: delete constructor
+ */
 ClientPlayer::~ClientPlayer() {
 }
 
+/**
+ * function name: sendNextMove
+ * input: int x, int y
+ * output: void
+ * operation: the function write the move of player to the socket
+ */
 void ClientPlayer::sendNextMove(int x, int y) {
     int xCpy = x;
     int yCpy = y;
@@ -28,7 +45,12 @@ void ClientPlayer::sendNextMove(int x, int y) {
         throw "Error writing move to socket";
     }
 }
-
+/**
+ * function name: readOrder
+ * input: void
+ * output: int
+ * operation: the function read the player's order from the socket
+ */
 int ClientPlayer::readOrder() {
 	// read the player's order from the socket
 	int order;
@@ -39,6 +61,12 @@ int ClientPlayer::readOrder() {
 	return order;
 }
 
+/**
+ * function name: chooseSquare
+ * input: vector of possible move of player, player current and player oppoent
+ * output: the square that player choose. 
+ * operation: 
+ */
 Square ClientPlayer::chooseSquare(vector<Square> possibleMoves, Player* current, Player* opponent) {
 	cout << type << ": It's your move. ";
 	// print the moves to the current player
@@ -61,7 +89,12 @@ Square ClientPlayer::chooseSquare(vector<Square> possibleMoves, Player* current,
 		cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 	}
 }
-
+/**
+ * function name: Board
+ * input: the board's size
+ * output: new object of board
+ * operation: constructor
+ */
 void ClientPlayer::noMove(Player* current, Player* opponent) {
     cout << current->getType() << " Has no possible moves. Play passes back to " << opponent->getType() << endl;
     int noMove = -2;
@@ -70,7 +103,12 @@ void ClientPlayer::noMove(Player* current, Player* opponent) {
         throw "Error writing move to socket";
     }
 }
-
+/**
+ * function name: Board
+ * input: the board's size
+ * output: new object of board
+ * operation: constructor
+ */
 void ClientPlayer::endGame() {
 	int end = -1;
 	int n = write(clientSocket, &end, sizeof(int));
