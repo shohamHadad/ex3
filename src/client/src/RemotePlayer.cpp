b096282cbebefa101383a44cd13bc974cb7d2c61
shohamHadad <33810472+shohamHadad@users.noreply.github.com>
2017-12-11 20:25:19 +0200

    Update RemotePlayer.cpp

--- src/client/src/RemotePlayer.cpp
+++ src/client/src/RemotePlayer.cpp
@@ -1,11 +1,28 @@
 #include "../include/RemotePlayer.h"
-
+/**
+ * function name: RemotePlayer()
+ * input: void
+ * output: new RemotePlayer
+ * operation: constctor
+ */
 RemotePlayer::RemotePlayer() {
 }
 
+/**
+ * function name: ~RemotePlayer()
+ * input: void
+ * output: delete RemotePlayer
+ * operation: delete constctor
+ */
 RemotePlayer::~RemotePlayer() {
 }
 
+/**
+ * function name: chooseSquare()
+ * input: vector<Square>
+ * output: Square
+ * operation: the function return the next move of the player
+ */
 Square RemotePlayer::chooseSquare(vector<Square> possibleMoves, Player* current, Player* opponent) {
 	cout << "Waiting for other player's move..." << endl;
 	return opponent->getNextMove();
