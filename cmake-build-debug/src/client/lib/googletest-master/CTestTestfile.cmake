a0b044b5cab2b9459fb67276e05000d43f6ec64f
Shir Kempinski <shirk9@walla.co.il>
2017-12-24 18:51:31 +0200

    no more cmake!!

--- cmake-build-debug/src/client/lib/googletest-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shoham/CLionProjects/ex3/src/client/lib/googletest-master
-# Build directory: /home/shoham/CLionProjects/ex3/cmake-build-debug/src/client/lib/googletest-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
