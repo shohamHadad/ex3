7bed844ca6807f36a01a0890dcd7494f69f421c1
shohamHadad <33810472+shohamHadad@users.noreply.github.com>
2018-01-03 02:35:33 +0200

    Update JoinCommand.cpp

--- src/client/src/JoinCommand.cpp
+++ src/client/src/JoinCommand.cpp
@@ -1,7 +1,19 @@
 #include "../include/JoinCommand.h"
 
+/**
+ * function name: JoinCommand
+ * input: gets a socket, and list of args
+ * output: new object of JoinCommand
+ * operation: constructor
+ */
 JoinCommand::JoinCommand(int socket): Command(socket) {}
 
+/**
+ * function name: execute
+ * input: string name
+ * output: int
+ * operation: The function join the player whit play exist
+ */
 int JoinCommand::execute(string name) {
     writeCommand("join", name);
     return executeFeedback(2);
