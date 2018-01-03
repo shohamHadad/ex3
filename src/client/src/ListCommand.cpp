1860ef2538fbefee2c47e1676d94274c6f56baa8
shohamHadad <33810472+shohamHadad@users.noreply.github.com>
2018-01-03 02:39:17 +0200

    Update ListCommand.cpp

--- src/client/src/ListCommand.cpp
+++ src/client/src/ListCommand.cpp
@@ -1,7 +1,19 @@
 #include "../include/ListCommand.h"
 
+/**
+ * function name: ListCommand
+ * input: gets a socket, and list of args
+ * output: new object of ListCommand
+ * operation: constructor
+ */
 ListCommand::ListCommand(int socket): Command(socket) {}
 
+/**
+ * function name: execute
+ * input: string name
+ * output: int
+ * operation: The function write to the client list of rooms game
+ */
 int ListCommand::execute(string name) {
     writeCommand("list_games", name);
     return executeFeedback(-1);
