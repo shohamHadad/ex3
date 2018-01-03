cee2754209b7450b677f60189f9b09fd27499d6d
shohamHadad <33810472+shohamHadad@users.noreply.github.com>
2018-01-03 02:45:35 +0200

    Update StartCommand.cpp

--- src/client/src/StartCommand.cpp
+++ src/client/src/StartCommand.cpp
@@ -1,7 +1,20 @@
 #include "../include/StartCommand.h"
 
+/**
+ * function name:StartCommand
+ * input: int socket
+ * output: new object of StartCommand
+ * operation: constructor
+ */
 StartCommand:: StartCommand(int socket): Command(socket) {}
 
+
+/**
+ * function name: execute
+ * input: string name
+ * output: int
+ * operation: The function write to the server a new name of room
+ */
 int StartCommand::execute(string name) {
     writeCommand("start", name);
     return executeFeedback(1);
